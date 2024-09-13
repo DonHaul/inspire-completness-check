c6d8fccf8a5534ff72b14254ced0e773f1b02ef4
DonHaul <ramiro.animus@gmail.com>
2024-09-13 12:46:57 +0200

    completeness

--- lib/arxiv_completness_check_script.py
+++ lib/arxiv_completness_check_script.py
@@ -145,9 +145,9 @@ def prepare_message(
 **ArXiv Harvest Check** started at {check_start_time}.
 Summary:\n"""
         + f"* **{len(arxiv_eprints)}** eprints published by"
-        f" ArXiv from {from_date} to {to_date},"
+        f" ArXiv from {from_date} to {to_date}, "
         f" **{number_of_holdingpen_matches}**"
-        f" received in total, **{len(inspire_eprints)}** in INSPIRE."
+        f" received in total2, **{len(inspire_eprints)}** in INSPIRE."
         + f"""
 
 {missing_article_info}
